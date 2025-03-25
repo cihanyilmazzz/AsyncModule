e6ce1ed7d905acadc156366215c23dbd7a379ee9
Cihan Yilmaz <chnrzgr@gmail.com>
2025-03-26 00:44:44 +0100

    Initial Commit

---
+++ AsyncModule/AsyncModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncModuleApp.swift
+//  AsyncModule
+//
+//  Created by Cihan Yilmaz on 26/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct AsyncModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncModuleTests/AsyncModuleTests.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncModuleTests.swift
+//  AsyncModuleTests
+//
+//  Created by Cihan Yilmaz on 26/03/2025.
+//
+
+import Testing
+@testable import AsyncModule
+
+struct AsyncModuleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
